d7b9f337053638dd1522185aa2f2e29e2c8fe449
ShalithAlahakoon <shalitha97068@gmail.com>
2022-04-03 07:52:56 +0530

    server configuration

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,26 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connect, connection as _connection } from 'mongoose';
-import { json } from 'body-parser';
-import cors from 'cors';
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(json());
+app.use(bodyParser.json());
 
 //connect to mongodb
 const URL = process.env.MONGODB_URL;
 
-connect(URL, {
+mongoose.connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+
 });
 
-const connection = _connection;
+const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('MongoDB database connection established successfully');
 });
